6d27021e42aeae13627bd48a8e985bde694ff661
John Chipoco-Campoverde <JohnC@Magenic.com>
2021-01-15 01:41:21 -0600

    first commit, configuration edits

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    production: false,
+    log: true,
+    flags: {
+        useNewHeader: true
+    },
+    name: 'dev',
+    apiUrl: 'http://angular-pics-dev'
+}
---
+++ src/environments/environment.qa.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    production: true,
+    log: true,
+    flags: {
+        useNewHeader: true
+    },
+    name: 'qa',
+    apiUrl: 'http://angular-pics-qa'
+}
